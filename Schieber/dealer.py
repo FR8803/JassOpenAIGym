ad0ec576c398e3e398a420ce9f784511bded30a7
FR8803 <61582118+FR8803@users.noreply.github.com>
2020-12-28 18:13:43 +0100

    Add files via upload

---
+++ Schieber/dealer.py
@@ -0,0 +1,20 @@
+from rlcard.games.jass.card import init_swiss_deck
+
+
+class JassDealer(object):
+    def __init__(self, np_random):
+        self.np_random = np_random
+        self.deck = init_swiss_deck()
+        self.shuffle()
+
+    def shuffle(self):
+        self.np_random.shuffle(self.deck)
+
+    def deal_cards(self, player, num):
+        for _ in range(num):
+            player.hand.append(self.deck.pop())
+        player.hand = sorted(player.hand)
+
+    def take_deck_back(self):
+        self.deck = init_swiss_deck()
+        self.shuffle()
